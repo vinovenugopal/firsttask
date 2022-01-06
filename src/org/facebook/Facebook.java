10d713913c402bbe3681d8ec4e28575c6e11fbcf
vinovenugopal <vinoth2202@gmail.com>
2022-01-06 10:36:02 +0530

    firsttask

---
+++ src/org/facebook/Facebook.java
@@ -0,0 +1,14 @@
+package org.facebook;
+
+public class Facebook {
+	
+	private void empInfo() {
+		System.out.println("vinoth");
+
+	}
+	public static void main(String[] args) {
+		Facebook f = new Facebook();
+		f.empInfo();
+	}
+	
+}
